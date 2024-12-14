bc2963e6ef163ef68c27dce950b1bc4ef6288554
Senthilnathan Marimuthu <sdssenthil@gmail.com>
2024-12-14 21:03:33 +0000

    My first Git Commit

---
+++ PhaRequests/Schemas/PhaRequests_Detail/PhaRequests_Detail.js
@@ -0,0 +1,8 @@
+define('PhaRequests_Detail', [], function () {
+	return {
+		entitySchemaName: 'PhaRequests',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
